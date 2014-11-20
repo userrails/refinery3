16107e112fb457289660435d578ca57cf4c1bb47
rails <rails@rails>
2014-11-20 11:43:15 +0530

    initial commit for refinery using rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PianoPlus::Application.config.secret_token = 'da54d776f23a4e25f7a3db7b3840d1d4f068d24181bdd252004d6f176121198a73e96b51d8b95d6f71d30e15cd4fb0c3b8d7a1970fc632db7039f7b1acb5e26f'
